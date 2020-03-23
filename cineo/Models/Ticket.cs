cac4afc7749168a8fad809e29efd6db86a4e753f
ry2223 <61561822+ry2223@users.noreply.github.com>
2020-03-23 13:33:53 +0100

    Dodanie kluczy obcych do modelu Ticket

--- cineo/Models/Ticket.cs
+++ cineo/Models/Ticket.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace cineo.Models
 {
     public class Ticket
     {
+        [Key]
         public int Id { get; set; }
+
+        [ForeignKey("Movie")]
+        public Movie Movie_Id { get; set; }
+
+        [ForeignKey("Seance")]
+        public Seance Seance_Id { get; set; }
 
         [Required]
         public string CinemaName { get; set; }
@@ -17,6 +25,7 @@ namespace cineo.Models
         public string MovieTitle { get; set; }
 
         [Required]
+        [Range(0,999999999999)]
         public long BarcodeNumber { get; set; }
 
         [Required]
